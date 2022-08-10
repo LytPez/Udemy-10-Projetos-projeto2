0ca7f82f5aba502e73a46d569345ccb2d94e04e4
Guilherme <guilhermeisk@hotmail.com>
2022-08-10 14:24:14 -0300

    alteração

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 função que conta as palavras, usando o .split() para contar quandos ' ' tem entre as palavras, assim contando quantas palavras existem, esta função é chamada ao clicar em um botao criado em html 
 */
-
+//o .split() pega os caracteres que você descrever e conta quantos tem. ' ' é um caracter
 
 function calcular(){
   let text=document.getElementById('textarea').value
